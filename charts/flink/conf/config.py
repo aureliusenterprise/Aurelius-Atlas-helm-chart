83147ce8920d309c20214aa72bac24bd89f3d433
Andreas <andreas.wombacher@aureliusenterprise.com>
2022-07-19 22:54:14 +0200

    updated configs

--- charts/flink/conf/config.py
+++ charts/flink/conf/config.py
@@ -1,16 +1,20 @@
 config = {
-        "atlas.server.url": "http://atlas.anwo.svc.cluster.local",
-    "atlas.server.port": 21000,
-    "kafka.bootstrap.server.hostname": "http://atlas.anwo.svc.cluster.local",
-    "kafka.bootstrap.server.port": 9027,
-    "kafka.consumer.group.id": "",
+    "atlas.server.url": "http://atlas.anwo.svc.cluster.local:21000/api/atlas",
+   "kafka.bootstrap.server.hostname": "atlas.anwo.svc.cluster.local",
+    "kafka.bootstrap.server.port": "9027",
+    "kafka.consumer.group.id": None,
     "atlas.audit.events.topic.name": "ATLAS_ENTITIES",
+    "enriched.events.topic.name": "ENRICHED_ENTITIES",
+    "determined.events.topic.name": "DETERMINED_CHANGE",
+    "exception.events.topic.name": "DEAD_LETTER_BOX",
+
+    "elastic_search_index" : "atlas-dev-test",
 
     "elastic_cloud_username" : "YOUR_ELASTIC_USER_NAME",
     "elastic_cloud_id" : "YOUR_ELASTIC_CLOUD_ID",
+    "elastic_base_endpoint" : "APP-SEACRH-HOSTNAME/api/as/v1",
 
-    "elastic_user": "elastic",
-    "elastic_passwd": "password",
-    "elastic_cloud_id": "cloud-id",
-    "elastic_search_passwd": "search-password",
+    "keycloak.server.url" : "reliusdev.westeurope.cloudapp.azure.com/anwo/auth/",
+    "keycloak.client.id" : "m4i_public",
+    "keycloak.realm.name": "m4i",    
 }
