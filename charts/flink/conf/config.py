0287dd893313aa038817c3a9f60c67b637956e1f
Andreas <andreas.wombacher@aureliusenterprise.com>
2022-08-24 01:59:51 +0200

    increased task manager memory and adjusted config to access elastic via the public URL
    the internal url has a self-signed certificate, which is not accepted by the elastichsearch client

--- charts/flink/conf/config.py
+++ charts/flink/conf/config.py
@@ -13,9 +13,12 @@ config = {
 
     "elastic.cloud.username" : "elastic",
     "elastic.cloud.id" : None,
-    "elastic.base.endpoint" : "elastic.{{ .Release.Namespace }}.svc.cluster.local:9200/api/as/v1",
-    "elastic.search.endpoint" : "http://elastic-search-es-http.{{ .Release.Namespace }}.svc.cluster.local:9200",
-    "elastic.enterprise.search.endpoint" : "http://enterprise-search-ent-http.{{ .Release.Namespace }}.svc.cluster.local:3002",
+    # "elastic.base.endpoint" : "elastic.{{ .Release.Namespace }}.svc.cluster.local:9200/api/as/v1",
+    # "elastic.search.endpoint" : "http://elastic-search-es-http.{{ .Release.Namespace }}.svc.cluster.local:9200",
+    # "elastic.enterprise.search.endpoint" : "http://enterprise-search-ent-http.{{ .Release.Namespace }}.svc.cluster.local:3002",
+    "elastic.base.endpoint" : "{{ .Values.external_hostname }}/{{ .Release.Namespace }}/elastic/api/as/v1",
+    "elastic.search.endpoint" : "https://{{ .Values.external_hostname }}:443/{{ .Release.Namespace }}/elastic",
+    "elastic.enterprise.search.endpoint" : "https://{{ .Values.external_hostname }}:443/{{ .Release.Namespace }}/app-search",
 
     "keycloak.server.url" : "https://{{ .Values.external_hostname }}/{{ .Release.Namespace }}/auth/",
     "keycloak.client.id" : "m4i_public",
