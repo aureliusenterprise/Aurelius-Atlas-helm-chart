a49970fcb13f0782e748e3610260027d56d934a0
Andreas <andreas.wombacher@aureliusenterprise.com>
2022-11-28 13:40:52 +0100

    made the external url configurable in a single variable for all pods

--- charts/flink/conf/config.py
+++ charts/flink/conf/config.py
@@ -19,11 +19,11 @@ config = {
     # "elastic.base.endpoint" : "elastic.{{ .Release.Namespace }}.svc.cluster.local:9200/api/as/v1",
     # "elastic.search.endpoint" : "http://elastic-search-es-http.{{ .Release.Namespace }}.svc.cluster.local:9200",
     # "elastic.enterprise.search.endpoint" : "http://enterprise-search-ent-http.{{ .Release.Namespace }}.svc.cluster.local:3002",
-    "elastic.base.endpoint" : "{{ .Values.external_hostname }}/{{ .Release.Namespace }}/elastic/api/as/v1",
-    "elastic.search.endpoint" : "https://{{ .Values.external_hostname }}:443/{{ .Release.Namespace }}/elastic",
-    "elastic.enterprise.search.endpoint" : "https://{{ .Values.external_hostname }}:443/{{ .Release.Namespace }}/app-search",
+    "elastic.base.endpoint" : "{{ .Values.global.external_hostname }}/{{ .Release.Namespace }}/elastic/api/as/v1",
+    "elastic.search.endpoint" : "https://{{ .Values.global.external_hostname }}:443/{{ .Release.Namespace }}/elastic",
+    "elastic.enterprise.search.endpoint" : "https://{{ .Values.global.external_hostname }}:443/{{ .Release.Namespace }}/app-search",
 
-    "keycloak.server.url" : "https://{{ .Values.external_hostname }}/{{ .Release.Namespace }}/auth/",
+    "keycloak.server.url" : "https://{{ .Values.global.external_hostname }}/{{ .Release.Namespace }}/auth/",
     "keycloak.client.id" : "m4i_public",
     "keycloak.realm.name": "m4i",    
 }
