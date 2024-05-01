d41f0a0bd317d93933d2133ff7c691aedd026463
matjgora <mateusz.gora@aureliusenterprise.com>
2024-05-01 15:18:42 +0200

    add import instructions

--- backup/export-atlas.py
+++ backup/export-atlas.py
@@ -5,9 +5,25 @@ from urlpath import URL
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--token", "-t", required=True, help="Atlas access token", type=str)
-    parser.add_argument("--base-url", "-u", default="https://aureliusdev.westeurope.cloudapp.azure.com/demo/atlas2/", help="Apache Atlas base url", type=URL)
-    parser.add_argument("--output", "-o", default="out.zip", help="Output zip file", type=str)
+    parser.add_argument(
+        "--token", "-t", required=True, help="Atlas access token", type=str
+    )
+    parser.add_argument(
+        "--base-url",
+        "-u",
+        default="https://aureliusdev.westeurope.cloudapp.azure.com/demo/atlas2/",
+        help="Apache Atlas base url",
+        type=URL,
+    )
+    parser.add_argument(
+        "--output", "-o", default="out.zip", help="Output zip file", type=str
+    )
+    parser.add_argument(
+        "--import-data",
+        "-i",
+        action="store_true",
+        help="When passed, the script will perform import of data (export by default)",
+    )
     return parser.parse_args()
 
 
@@ -15,34 +31,53 @@ def get_entity_types(base_url, base_headers={}):
     url = base_url / "v2/types/typedefs"
     response = requests.get(url.as_uri(), headers=base_headers)
     data = response.json()
-    return [entity["name"] for entity in data["entityDefs"] if entity["category"] == "ENTITY"]
+    return [
+        entity["name"]
+        for entity in data["entityDefs"]
+        if entity["category"] == "ENTITY"
+    ]
 
 
 def export(entity_types, base_url, output, base_headers={}):
     headers = base_headers.copy()
     body = {"itemsToExport": []}
     for entity in entity_types:
-        body["itemsToExport"].append({"typeName": entity, "uniqueAttributes": {"qualifiedName": ".*"}})
+        body["itemsToExport"].append(
+            {"typeName": entity, "uniqueAttributes": {"qualifiedName": ".*"}}
+        )
 
     body["options"] = {"matchType": "matches"}
     headers["Content-Type"] = "application/json"
     headers["Cache-Control"] = "no-cache"
     url = base_url / "admin/export"
-    
+
     response = requests.post(url.as_uri(), json=body, headers=headers)
     with open(output, "wb") as handler:
         handler.write(response.content)
 
 
+def import_data(base_url, export_output, base_headers):
+    headers = base_headers.copy()
+
+    headers["Cache-Control"] = "no-cache"
+    url = base_url / "admin/import"
+    files = {"data": open(export_output, "rb")}
+
+    requests.post(url.as_uri(), files=files, headers=headers)
+
+
 def main():
     args = parse_args()
     base_url = args.base_url / "api/atlas"
-    headers = { "Authorization": f"Bearer {args.token}" }
-    print(headers)
-    print("Getting all entity types")
-    entity_types = get_entity_types(base_url, headers)
-    print("Exporting entities")
-    export(entity_types, base_url, args.output, headers)
+    headers = {"Authorization": f"Bearer {args.token}"}
+    if args.import_data:
+        print("Importing entities")
+        print(import_data(base_url, args.output, headers))
+    else:
+        print("Getting all entity types")
+        entity_types = get_entity_types(base_url, headers)
+        print("Exporting entities")
+        export(entity_types, base_url, args.output, headers)
     print("Done!")
 
 
